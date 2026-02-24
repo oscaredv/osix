5568d76ac19567cc1f9506be5d76d2be7304e30e
Oscar Endre Edvardsen <oscar.edvardsen@gmail.com>
2026-02-24 21:40:13 +0100

    Lots of work on bin/sh to make it more usable

--- bin/sh.c
+++ bin/sh.c
@@ -16,19 +16,25 @@
 #define PROMPT_SIZE 16
 #define MAX_ARGS 256
 #define MAX_CMDS 16
+#define MAX_PIPES 16
 
 struct Command {
   char *argv[MAX_ARGS];
+  char quoted[MAX_ARGS];
   char *input;
   char *output;
   int append;
 };
-struct Command cmds[MAX_CMDS];
+
+struct Pipeline {
+  struct Command cmds[MAX_CMDS];
+  int cmds_count;
+};
+
+struct Pipeline pipelines[MAX_PIPES];
+int pipelines_count;
 
 char prompt[PROMPT_SIZE] = "$ ";
-
-char *allocs[MAX_ARGS];
-int alloc_count;
 
 int internal_cmd(char *argv[]) {
   if (!strcmp(argv[0], "exit")) {
@@ -49,6 +55,16 @@ int internal_cmd(char *argv[]) {
     return 1;
   }
 
+  for (int i = 0; argv[0][i]; i++) {
+    // Set variable
+    if (argv[0][i] == '=') {
+      argv[0][i] = 0;
+      char *varname = argv[0];
+      char *value = &argv[0][i + 1];
+      setenv(varname, value, 1);
+      return 1;
+    }
+  }
   return 0;
 }
 
@@ -83,14 +99,6 @@ int pattern_match(const char *pattern, const char *s) {
     pattern++;
   }
   return !*pattern;
-}
-
-char *alloc_string(const char *s) {
-  int len = strlen(s) + 1;
-  char *p = malloc(len);
-  strncpy(p, s, len);
-  allocs[alloc_count++] = p;
-  return p;
 }
 
 void explode_pattern(const char *base, const char *pattern, int *argc, char *argv[]) {
@@ -132,7 +140,7 @@ void explode_pattern(const char *base, const char *pattern, int *argc, char *arg
                 snprintf(fullname, sizeof(fullname), "%s/%s", base, dir.d_name);
               }
 
-              argv[*argc] = alloc_string(fullname);
+              argv[*argc] = strdup(fullname);
               (*argc)++;
             } else {
               char next_base[strlen(base) + 1 + strlen(dir.d_name) + 1];
@@ -154,28 +162,30 @@ void explode_pattern(const char *base, const char *pattern, int *argc, char *arg
   }
 }
 
-void debug_pipeline(struct Command *cmds, int n) {
-  for (int i = 0; i < n; i++) {
-    printf("  Command %d: input=%s output=%s append=%d\n", i, cmds[i].input, cmds[i].output, cmds[i].append);
-    for (int a = 0; a < MAX_ARGS && cmds[i].argv[a] != NULL; a++) {
-      printf("    argv[%d]: %s\n", a, cmds[i].argv[a]);
-    }
-    if (i < n - 1) {
-      printf("   |\n");
+void glob(struct Pipeline *pipeline) {
+  for (int c = 0; c < pipeline->cmds_count; c++) {
+    int a = 1;
+    while (pipeline->cmds[c].argv[a] != NULL) {
+      int old_argc = a;
+      if (!pipeline->cmds[c].quoted[a]) {
+        char *arg = pipeline->cmds[c].argv[a];
+        explode_pattern("", arg, &a, pipeline->cmds[c].argv);
+      }
+      if (a == old_argc)
+        a++;
     }
   }
-  printf("\n");
 }
 
-void exec_pipeline(struct Command *cmds, int n) {
-  int pipes[2 * (n - 1)];
+void exec_pipeline(struct Pipeline *pipeline) {
+  int pipes[2 * (pipeline->cmds_count - 1)];
 
-  for (int i = 0; i < n - 1; i++) {
+  for (int i = 0; i < pipeline->cmds_count - 1; i++) {
     pipe(pipes + i * 2);
   }
 
-  for (int i = 0; i < n; i++) {
-    if (internal_cmd(cmds[i].argv)) {
+  for (int i = 0; i < pipeline->cmds_count; i++) {
+    if (internal_cmd(pipeline->cmds[i].argv)) {
       continue;
     }
 
@@ -185,60 +195,70 @@ void exec_pipeline(struct Command *cmds, int n) {
       if (i > 0) {
         dup2(pipes[(i - 1) * 2], STDIN_FILENO);
       }
-      if (i < n - 1) {
+      if (i < pipeline->cmds_count - 1) {
         dup2(pipes[i * 2 + 1], STDOUT_FILENO);
       }
 
       // Redirections
-      if (cmds[i].input) {
-        int fd = open(cmds[i].input, O_RDONLY);
+      if (pipeline->cmds[i].input) {
+        int fd = open(pipeline->cmds[i].input, O_RDONLY);
         dup2(fd, STDIN_FILENO);
         close(fd);
       }
 
-      if (cmds[i].output) {
-        int flags = O_WRONLY | O_CREAT | (cmds[i].append ? O_APPEND : O_TRUNC);
-        int fd = open(cmds[i].output, flags, 0644);
+      if (pipeline->cmds[i].output) {
+        int flags = O_WRONLY | O_CREAT | (pipeline->cmds[i].append ? O_APPEND : O_TRUNC);
+        int fd = open(pipeline->cmds[i].output, flags, 0644);
         dup2(fd, STDOUT_FILENO);
         close(fd);
       }
 
       // close all pipes
-      for (int j = 0; j < 2 * (n - 1); j++) {
+      for (int j = 0; j < 2 * (pipeline->cmds_count - 1); j++) {
         close(pipes[j]);
       }
 
       // Execute command
-      int ret = execvp(cmds[i].argv[0], cmds[i].argv);
-      perror(cmds[i].argv[0]);
+      int ret = execvp(pipeline->cmds[i].argv[0], pipeline->cmds[i].argv);
+      perror(pipeline->cmds[i].argv[0]);
       exit(ret);
     }
   }
 
   // parent closes pipes
-  for (int i = 0; i < 2 * (n - 1); i++) {
+  for (int i = 0; i < 2 * (pipeline->cmds_count - 1); i++) {
     close(pipes[i]);
   }
 }
 
-char *resolve_var(char *p) {
-  if (p[0] != '$')
-    return p;
+char *expand_variable(char *arg) {
+  // TODO: Handle ${VAR} syntax and scan entire word for variables, not just the first character
+  // TODO: Handle escaping of $ with \$
+  // TODO: Handle variables in redirection targets
+  // TODO: Handle ~ expansion to $HOME
+  if (arg[0] != '$')
+    return arg;
 
-  char *var = getenv(&p[1]);
-  if (var)
-    return var;
-  return p;
+  char *var = getenv(&arg[1]);
+  if (var) {
+    free(arg);
+    return strdup(var);
+  }
+  free(arg);
+  return strdup("");
+}
+
+void expand_variables(struct Pipeline *pipeline) {
+  for (int c = 0; c < pipeline->cmds_count; c++) {
+    for (int a = 0; pipeline->cmds[c].argv[a] != NULL; a++) {
+      char *arg = pipeline->cmds[c].argv[a];
+      pipeline->cmds[c].argv[a] = expand_variable(arg);
+    }
+  }
 }
 
 char *parse_redir(struct Command *cmd, char *p) {
-  int input = 0;
-
-  if (*p == '<')
-    input = 1;
-
-  *p++ = 0;
-
+  char input = *p++ == '<' ? 1 : 0;
   if (*p == '>') {
     input = 0;
     cmd->append = 1;
@@ -248,113 +268,25 @@ char *parse_redir(struct Command *cmd, char *p) {
   while (*p == ' ')
     p++;
 
-  if (input)
-    cmd->input = p;
-  else
-    cmd->output = p;
-
-  while (*p != ' ' && *p != 0 && *p != '<' && *p != '>')
+  char *target = p;
+  while (*p != ' ' && *p != 0 && *p != '<' && *p != '>' && *p != '|' && *p != ';')
     ++p;
 
+  char old = *p;
+  *p = 0;
+  target = expand_variable(strdup(target));
+  if (input) {
+    cmd->input = target;
+  } else {
+    cmd->output = target;
+  }
+
+  *p = old;
   if (*p == '<' || *p == '>') {
     p = parse_redir(cmd, p);
-  } else if (*p != 0) {
-    *p++ = 0;
   }
-
-  if (cmd->input)
-    cmd->input = resolve_var(cmd->input);
-  if (cmd->output)
-    cmd->output = resolve_var(cmd->output);
 
   return p;
-}
-
-void parse_cmd(struct Command *cmd, char *cmdline) {
-  int argc = 0;
-  char *p = cmdline;
-  while (*p != 0) {
-    while (*p == ' ')
-      p++;
-    if (*p == 0)
-      break;
-
-    if (*p == '<' || *p == '>') {
-      p = parse_redir(cmd, p);
-      continue;
-    }
-
-    cmd->argv[argc] = p;
-    char pattern = 0;
-    while (*p != ' ' && *p != 0 && *p != '<' && *p != '>') {
-      if (*p == '*' || *p == '?')
-        pattern = 1;
-      ++p;
-    }
-    if (*p == '<' || *p == '>') {
-      p = parse_redir(cmd, p);
-    }
-    if (*p != 0)
-      *p++ = 0;
-
-    // Environment variable expansion
-    cmd->argv[argc] = resolve_var(cmd->argv[argc]);
-
-    if (pattern) {
-      int old_argc = argc;
-      explode_pattern("", cmd->argv[argc], &argc, cmd->argv);
-      if (argc == old_argc)
-        argc++;
-    } else {
-      argc++;
-    }
-  }
-  cmd->argv[argc] = NULL;
-}
-
-void parse_pipeline(char *cmdline) {
-  memset(cmds, 0, sizeof(cmds));
-  char *p = cmdline;
-  int c = 0;
-  while (*p != 0) {
-    while (*p == ' ')
-      p++;
-    char *start = p;
-    while (*p != '|' && *p != 0)
-      p++;
-    if (*p == '|')
-      *p++ = 0;
-
-    parse_cmd(&cmds[c++], start);
-  }
-
-  exec_pipeline(cmds, c);
-
-  // Free allocated strings
-  for (int i = 0; i < alloc_count; i++) {
-    free(allocs[i]);
-  }
-  alloc_count = 0;
-
-  // Wait for children
-  for (int i = 0; i < c; i++) {
-    wait(NULL);
-  }
-}
-
-void parse_separator(char *cmdline) {
-  char *p = cmdline;
-  while (*p != 0) {
-    while (*p == ' ')
-      p++;
-    char *start = p;
-    while (*p != ';' && *p != 0)
-      p++;
-    if (*p == ';')
-      *p++ = 0;
-
-    parse_pipeline(start);
-  }
 }
 
 void sighandler(int signum) {
@@ -362,14 +294,134 @@ void sighandler(int signum) {
   printf("^C\n");
 }
 
+void copy_token(char *dst, const char *src, int len) {
+  int j = 0;
+  char quote = 0;
+  for (int i = 0; i < len; i++) {
+    if (src[i] == '\"') {
+      quote = !quote;
+    }
+
+    if (src[i] == '\\' && i + 1 < len && !quote && (src[i + 1] == ' ' || src[i + 1] == '\\' || src[i + 1] == '\"')) {
+      dst[j++] = src[++i];
+    } else if (src[i] == '\"') {
+      // skip quotes
+    } else {
+      dst[j++] = src[i];
+    }
+  }
+  dst[j] = 0;
+}
+
+void reset_pipelines() {
+  for (int p = 0; p < MAX_PIPES; p++) {
+    pipelines[p].cmds_count = 0;
+    for (int c = 0; c < MAX_CMDS; c++) {
+      for (int a = 0; a < MAX_ARGS; a++) {
+        pipelines[p].cmds[c].argv[a] = NULL;
+        pipelines[p].cmds[c].quoted[a] = 0;
+      }
+      pipelines[p].cmds[c].input = NULL;
+      pipelines[p].cmds[c].output = NULL;
+      pipelines[p].cmds[c].append = 0;
+    }
+  }
+  pipelines_count = 0;
+}
+
+void tokenize(char *cmdline) {
+  char *p = cmdline;
+  char quote = 0;
+
+  int pipe_index = 0;
+  int cmd_index = 0;
+  int arg_index = 0;
+
+  while (*p != 0) {
+    // Skip leading whitespace
+    while (isspace(*p))
+      p++;
+
+    if (*p == 0)
+      break;
+
+    char *start = p;
+    // scan string until next unescaped whitespace or end of string
+    while ((!isspace(*p) || quote == 1) && *p != 0 && *p != ';' && *p != '|' && *p != '<' && *p != '>') {
+      char c = *p++;
+      if (c == '\\') {
+        ++p;
+      } else if (c == '\"') {
+        quote = !quote;
+      }
+    }
+
+    if (p != start) {
+      // Copy token to argv
+      if (arg_index == 0) {
+        if (cmd_index == 0) {
+          pipelines_count++;
+        }
+        pipelines[pipe_index].cmds_count++;
+      }
+      pipelines[pipe_index].cmds[cmd_index].argv[arg_index] = malloc(p - start + 1);
+      copy_token(pipelines[pipe_index].cmds[cmd_index].argv[arg_index], start, p - start);
+      pipelines[pipe_index].cmds[cmd_index].quoted[arg_index] = *start == '\"';
+      arg_index++;
+    }
+
+    if (*p == '|') {
+      cmd_index++;
+      arg_index = 0;
+    } else if (*p == ';') {
+      pipelines[pipe_index].cmds_count = ++cmd_index;
+      pipe_index++;
+      cmd_index = 0;
+      arg_index = 0;
+    } else if (*p == '<' || *p == '>') {
+      p = parse_redir(&pipelines[pipe_index].cmds[cmd_index], p);
+    }
+
+    if (*p != 0)
+      *p++ = 0;
+  }
+}
+
+void debug_dump(struct Pipeline *pipeline) {
+  printf("Pipeline:\n");
+  for (int c = 0; c < pipeline->cmds_count; c++) {
+    printf("  Command %d:\n", c);
+    for (int a = 0; pipeline->cmds[c].argv[a] != NULL; a++) {
+      printf("    argv[%d]: '%s' quoted=%d in=%s out=%s\n", a, pipeline->cmds[c].argv[a], pipeline->cmds[c].quoted[a],
+             pipeline->cmds[c].input ? pipeline->cmds[c].input : "NULL",
+             pipeline->cmds[c].output ? pipeline->cmds[c].output : "NULL");
+    }
+  }
+}
+
+void wait_for_children() {
+  while (wait(NULL) > 0)
+    ;
+}
+
+void free_pipeline(struct Pipeline *pipeline) {
+  for (int c = 0; c < pipeline->cmds_count; c++) {
+    for (int a = 0; pipeline->cmds[c].argv[a] != NULL; a++) {
+      free(pipeline->cmds[c].argv[a]);
+      pipeline->cmds[c].argv[a] = NULL;
+    }
+  }
+}
+
 int main(void) {
   signal(SIGINT, sighandler);
-  alloc_count = 0;
 
   if (getuid() == 0) {
     strncpy(prompt, "# ", PROMPT_SIZE);
   }
 
+  // TODO: IFS handling: Support for setting and using IFS to control word splitting behavior
+  // TODO: Sub shell $(cmd): Execute cmd in a subshell and replace with its output
   char cmdline[MAX_LINE];
   while (1) {
     printf("%s", prompt);
@@ -381,7 +433,17 @@ int main(void) {
       break;
     } else if (len > 0) {
       cmdline[len - 1] = 0;
-      parse_separator(cmdline);
+      reset_pipelines();
+      tokenize(cmdline);
+
+      for (int p = 0; p < pipelines_count; p++) {
+        glob(&pipelines[p]);
+        expand_variables(&pipelines[p]);
+        // debug_dump(&pipelines[p]);
+        exec_pipeline(&pipelines[p]);
+        wait_for_children();
+        free_pipeline(&pipelines[p]);
+      }
     }
   }
 
